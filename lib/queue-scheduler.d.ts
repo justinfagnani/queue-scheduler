65f9772f0b1c72054834ac2be8e5f334e13401aa
Justin Fagnani <justinfagnani@google.com>
2018-08-15 14:45:59 -0700

    Remove files that should be ignored

--- lib/queue-scheduler.d.ts
+++
@@ -1,8 +0,0 @@
-import { TaskQueue } from './task-queue.js';
-/**
- * An object that controls when tasks are executed from a queue or set of
- * queues.
- */
-export interface QueueScheduler {
-    schedule(queue: TaskQueue): void;
-}
